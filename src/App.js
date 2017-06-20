00ce029568e62122eb591414bf041ede66bedc39
Jules Leow <jleow@iupui.edu>
2017-06-20 11:45:00 -0400

    Tie input to github component state

--- src/App.js
+++ src/App.js
@@ -11,15 +11,14 @@ class App extends Component {
         <div className="App-header">
           <div className="App-headings">
             <h3>Ain't no party like an</h3>
-            <h1>API PARTY</h1>
+            <h1>API Party</h1>
           </div>
-
-        <ul className="nav-links">
-          <li>
-            <NavLink to='/github'>Github API</NavLink>
-          </li>
-        </ul>
-      </div>
+          <ul className="nav-links">
+            <li>
+              <NavLink to='/github'>Github API</NavLink>
+            </li>
+          </ul>
+        </div>
         <Switch>
           <Route path='/github' component={Github} />
           <Route render={() => <p>To get started, click one of the links above</p>} />
@@ -28,5 +27,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
